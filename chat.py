1e70022864453707558441a4a3ca2336be27e3e2
robmcdonald5 <mcdonald.xman.joshua@gmail.com>
2024-10-24 23:01:15 -0500

    more default Flask changes

--- chat.py
+++ chat.py
@@ -582,4 +582,5 @@ def serve_static(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
