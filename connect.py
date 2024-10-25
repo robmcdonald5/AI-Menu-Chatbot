bc73b4db29cb81e2a10529ae84ee1a087a37bd15
robmcdonald5 <mcdonald.xman.joshua@gmail.com>
2024-10-24 21:34:52 -0500

    updated b/c updated

--- connect.py
+++ connect.py
@@ -1,11 +1,8 @@
-import os
+# connect.py
 from db import Database
 
-# Get environment variables
-db_host = os.getenv('DB_HOST', 'localhost')
-db_user = os.getenv('DB_USER', 'user')
-db_pass = os.getenv('DB_PASS', 'password')
-database_name = 'ChipotleMenu'
+# Initialize the Database object with only the database name
+database = Database('ChipotleMenu')
 
-# Initialize the Database object with connection parameters
-database = Database(database_name, host=db_host, user=db_user, password=db_pass)
+# Connect to MongoDB
+database.connect()
